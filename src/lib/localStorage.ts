7b944e19a5e0f70077e16ac227c008c40e79d35a
Redbiidd.sun <38393603+redbiiddsun@users.noreply.github.com>
2024-01-01 22:16:23 +0700

    refactor: add return to accessLocalStorage and remove space

--- src/lib/localStorage.ts
+++ src/lib/localStorage.ts
@@ -1,16 +1,22 @@
-import { Url } from "@prisma/client"
+import { Url } from "@prisma/client";
 
-export const accessLocalStorage = () => JSON.parse(localStorage.getItem('shortener-data') || '[]')
+export const accessLocalStorage = () => {
+    let resultStorage: Url[] = JSON.parse(
+        localStorage.getItem("shortener-data") || "[]"
+    );
+
+    return resultStorage;
+};
 
 export const storeLocalStorage = (data: Url | null) => {
+    let urlHistory = accessLocalStorage();
 
-  let urlHistory = accessLocalStorage()
+    if (data != null) {
+        urlHistory.push(data);
+    }
 
-  if (data != null) {
-    urlHistory.push(data)
-  }
+    localStorage.setItem("shortener-data", JSON.stringify(urlHistory));
+};
 
-  localStorage.setItem('shortener-data', JSON.stringify(urlHistory))
-}
-
-export const removeLocalStorage = () => localStorage.removeItem('shortener-data')
+export const removeLocalStorage = () =>
+    localStorage.removeItem("shortener-data");
